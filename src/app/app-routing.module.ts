26469d8825ae610fa13d434db22d122ea7deaa81
Vipin Yadav <imvpn22@gmail.com>
2019-03-06 01:35:21 +0530

    minor changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { IssuesComponent } from './issues/issues.component';
 import { IssueDetailComponent } from './issue-detail/issue-detail.component';
 import { IssueEditComponent } from './issue-edit/issue-edit.component';
+import { IssueAddComponent } from './issue-add/issue-add.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/issues', pathMatch: 'full' },
   { path: 'issues', component: IssuesComponent},
-  { path: 'view/:id', component: IssueDetailComponent },
-  { path: 'edit/:id', component: IssueEditComponent },
+  { path: 'issue/:id/view', component: IssueDetailComponent },
+  { path: 'issue/:id/edit', component: IssueEditComponent },
+  { path: 'issue/add', component: IssueAddComponent }
 ];
 
 @NgModule({
